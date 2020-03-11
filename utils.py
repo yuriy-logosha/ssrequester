58745121e81b38ad478245dc22c943c7739d8efe
yura <yuriy.logosha@gmail.com>
2020-03-11 23:59:16 +0200

    Update utils.

--- utils.py
+++ utils.py
@@ -64,6 +64,7 @@ class MyHTMLParser(HTMLParser):
         self.path = []
         self.data = []
         self.valid_tags = []
+        self.skip_tags = []
         self.parsers = {}
         self.current_tag = None
         self.is_current_tag_valid = False
@@ -79,6 +80,8 @@ class MyHTMLParser(HTMLParser):
         self.is_current_tag_valid = self.valid(tag)
         self.current_tag = tag
         if not self.is_current_tag_valid:
+            return
+        if self.is_skip(tag):
             return
         self.data.append((tag, attrs))
         super(self.__class__, self).handle_starttag(tag, attrs)
@@ -116,8 +119,12 @@ class MyHTMLParser(HTMLParser):
     def valid(self, tag):
         if len(self.valid_tags) > 0:
             return True if tag in self.valid_tags else False
-        else:
-            return True
+        return True
+
+    def is_skip(self, tag):
+        if len(self.skip_tags) > 0:
+            return True if tag in self.skip_tags else False
+        return False
 
     def feed_and_return(self, data):
         self.feed(data)
